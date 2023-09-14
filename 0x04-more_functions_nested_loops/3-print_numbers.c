7cd9715ae682976426c905b2710bb25b207851d1
Smilingbull <sossick2020@gmail.com>
2023-09-14 00:53:19 -0700

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * print_numbers - Print the numbers since 0 up to 9
+  *
+  * Return: The numbers since 0 up to 9
+  */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar('0' + a);
+	}
+	_putchar('\n');
+}

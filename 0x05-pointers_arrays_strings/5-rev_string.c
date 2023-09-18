b9a371dfc794e8b466f3b606a30a2216ad7e3ee9
Smilingbull <sossick2020@gmail.com>
2023-09-18 14:53:10 -0700

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  * rev_string - Reverses a string
+  * @s: The string to be modified
+  *
+  * Return: void
+  */
+void rev_string(char *s)
+{
+	int i, j;
+	char r[1000];
+
+
+	i = j = 0;
+	while (s[i] != '\0')
+	{
+		r[i] = s[i];
+		i++;
+	}
+	i--;
+	while (i >= 0)
+	{
+		s[i] = r[j];
+		i--;
+		j++;
+	}
+	s[j++] = '\0';
+}

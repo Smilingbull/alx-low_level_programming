3a720ea26dc4a8720ce40532f98b00747c6713e9
Smilingbull <sossick2020@gmail.com>
2023-12-08 05:19:45 -0800

    test3

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,10 +2,10 @@
 #include "listis.h"
 
 size_t print_dlistint(const dlistint_t *h) {
-    size_t count = 0;
+    size_t num = 0;
 
     if (h == NULL)
-		return (count);
+		return (num);
 
 	while (h->prev != NULL)
 		h = h->prev;
@@ -13,8 +13,8 @@ size_t print_dlistint(const dlistint_t *h) {
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		count += 1;
+		num += 1;
 	}
-	return (count);
+	return (num);
 }
 

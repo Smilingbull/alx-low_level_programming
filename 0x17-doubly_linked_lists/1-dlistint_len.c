15af57f148ad60291f09739c835ab5f7e9c120a4
Smilingbull <sossick2020@gmail.com>
2023-12-08 06:05:30 -0800

    tester

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -6,8 +6,9 @@
  * Return: number of elements
  */
 
-size_t dlistint_len(const dlistint_t *h) {
-    size_t count = 0;
+size_t dlistint_len(const dlistint_t *h) 
+{
+        size_t count = 0;
 
     while (h != NULL) {
         count++;
